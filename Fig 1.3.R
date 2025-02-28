f84f423bc025eecda6cb6ca1763af32d2a1d24d1
Dade-00 <davidesacchi1812@gmail.com>
2025-02-28 10:24:35 +0100

    Update Fig 1.3.R

--- Fig 1.3.R
+++ Fig 1.3.R
@@ -5,7 +5,6 @@ library(quantreg)
 library(ggplot2)
 library(dplyr)
 
-# Rimuovere valori non validi
 Salaries <- Salaries %>%
   filter(is.finite(log(yrs.service)), is.finite(log(salary))) %>%
   mutate(
@@ -13,13 +12,13 @@ Salaries <- Salaries %>%
     experience_group = cut(yrs.service, breaks=c(0,4,8,13,18,23,29,36,65), include.lowest=TRUE)
     )
 
-# Modelli di regressione quantilica log-lineare
+# log linear regression quantile models
 tau_values <- c(0.25, 0.5, 0.75)
 models <- lapply(tau_values, function(tau) {
   rq(log(salary) ~ log_exp, data = Salaries, tau = tau)
 })
 
-# Predizioni per ogni quantile
+# Predictions
 log_exp_seq <- seq(min(Salaries$log_exp, na.rm = TRUE), max(Salaries$log_exp, na.rm = TRUE), length.out = 100)
 predictions <- do.call(rbind, lapply(1:length(models), function(i) {
   data.frame(
@@ -29,11 +28,11 @@ predictions <- do.call(rbind, lapply(1:length(models), function(i) {
   )
 }))
 
-# Creazione del grafico
+# Plot
 ggplot(Salaries, aes(x=yrs.service, y = salary)) +
   geom_boxplot(aes(group = experience_group),
-               alpha = 0.7, fill = "gray80",outlier.shape = NA) +  # Boxplot uniformi
-  geom_line(data = predictions, aes(x = exp(log_exp), y = salary_pred, color = factor(tau)), size = 1) +  # Curve allineate
+               alpha = 0.7, fill = "gray80",outlier.shape = NA) +  
+  geom_line(data = predictions, aes(x = exp(log_exp), y = salary_pred, color = factor(tau)), size = 1) + 
   labs(
     title = "Salary Distribution with Log-Linear Quantile Regression",
     x = "Years of Service",
