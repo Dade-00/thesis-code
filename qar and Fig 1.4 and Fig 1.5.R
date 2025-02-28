7780e0c8c55dea5a2b7ac19080f192a64d734edd
Dade-00 <davidesacchi1812@gmail.com>
2025-02-28 10:25:42 +0100

    Add files via upload

---
+++ qar and Fig 1.4 and Fig 1.5.R
@@ -0,0 +1,38 @@
+setwd("C:/Users/acer/OneDrive - Alma Mater Studiorum Università di Bologna/Desktop/Unifi/stat economica/Progetto Presenti")
+data=read.table("LFPR dataset.csv",header=T,sep=",")
+names(data)=c("Date","Rate")
+attach(data)
+Tempo=as.Date(x=Date,format="%Y-%m-%d")
+start=as.numeric(c(format(Tempo[1],"%Y"),format(Tempo[1],"%m")))
+y=ts( data = data$Rate, start = start, frequency = 12)
+
+
+## altra destagionalizzazione
+library(forecast)
+stl_decomp <- stl(y, s.window = "periodic")
+y_adj_stl <- seasadj(stl_decomp)
+
+
+library(quantreg)
+
+# Lag della variabile indipendente
+y_lag <- stats::lag(y_adj_stl, +1)  
+
+# Rimuove NA iniziale causato dal lag
+df <- data.frame(y_lag = y_lag[-length(y_lag)],y_adj_stl = y_adj_stl[-1])
+
+
+
+
+taus=c(0.01,seq(0.05,0.95,0.05),0.99)
+
+par(mfrow=c(1,2))
+plot(x=Tempo,y=y_adj_stl, type = "l", col = "red", main = "Labor Force Participation Rate", ylab = "% Rate",xlab="Time")
+plot(df,type="n",main="QAR(1)", xlab="(Lagged) LFPR",ylab="LFPR")
+points(df,cex=0.5,col="blue")
+for (i in 1:length(taus)){
+  abline(rq(y_adj_stl ~ y_lag,tau=taus[i],data = df),col="gray")
+}
+
+fit1=summary(rq(y_adj_stl ~ y_lag, tau = 2:98/100, data = df))
+plot(fit1,mfrow=c(1,2))
